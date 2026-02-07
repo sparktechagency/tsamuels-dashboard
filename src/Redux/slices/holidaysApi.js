d713a4d0c4b77cac3b356ba4b3c6cd2c2b9d7c41
nrshakib <nrshakib007@gmail.com>
2026-02-07 09:02:05 +0600

    Integrated Holidays API

--- src/Redux/slices/holidaysApi.js
+++ src/Redux/slices/holidaysApi.js
@@ -15,7 +15,54 @@ const holidaysApi = baseApi.injectEndpoints({
       },
       providesTags: ["holidays"],
     }),
+    createHoliday: builder.mutation({
+      query: (data) => {
+        const accessToken = sessionStorage.getItem("accessToken");
+        return {
+          url: "/holidays/create",
+          method: "post",
+          body: data,
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+      invalidatesTags: ["holidays"],
+    }),
+    editHoliday: builder.mutation({
+      query: ({ data, id }) => {
+        const accessToken = sessionStorage.getItem("accessToken");
+        return {
+          url: `/holidays/update/${id}`,
+          method: "patch",
+          body: data,
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+      invalidatesTags: ["holidays"],
+    }),
+    deleteHoliday: builder.mutation({
+      query: ({ id }) => {
+        console.log("delete api id", id);
+        const accessToken = sessionStorage.getItem("accessToken");
+        return {
+          url: `/holidays/delete/${id}`,
+          method: "delete",
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+        };
+      },
+      invalidatesTags: ["holidays"],
+    }),
   }),
 });
 
-export const { useGetAllHolidaysDataQuery } = holidaysApi;
+export const {
+  useGetAllHolidaysDataQuery,
+  useCreateHolidayMutation,
+  useEditHolidayMutation,
+  useDeleteHolidayMutation,
+} = holidaysApi;
